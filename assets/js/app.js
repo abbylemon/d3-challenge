09ff59a1a86da84cbdd115e148d7910dd7b155bb
physics_nerd <abby.lemon22@gmail.com>
2020-04-01 17:14:37 -0500

    fixed d3 select spelling error, need to get plot to show on page, fix d3 tool tip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
 // @TODO: YOUR CODE HERE!
 
-var svgWidth = 1000;
+var svgWidth = 960;
 var svgHeight = 500;
 
-var margin  = {
+var margin = {
     top: 20,
     right: 40,
     bottom: 60,
-    left:50
-};
+    left: 100
+  };
 
 var width = svgWidth = margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-var svg = d3.selec("#scatter")
+var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
@@ -22,7 +22,7 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-d3.csv("./assets/data/data.csv").then(function (stateStats) {
+d3.csv("./assets/data/data.csv").then(function(stateStats) {
     console.log(stateStats);
 
     // plot smokes vs age
@@ -30,16 +30,18 @@ d3.csv("./assets/data/data.csv").then(function (stateStats) {
         data.smokes = +data.smokes;
         data.age = +data.age;
     });
+    // console.log(stateStats.smokes);
+    // console.log(stateStats.age);
 
     var xLinearScale = d3.scaleLinear()
-        .domain(d3.extend(stateStats, d => d.age))
+        .domain([0, 1.1 * d3.max(stateStats, d => d.age)])
         .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(stateStats, d => d.age)])
         .range([height, 0]);
 
     var bottomAxis = d3.axisBottom(xLinearScale);
-
     var leftAxis = d3.axisLeft(yLinearScale);
 
     chartGroup.append("g")
@@ -53,15 +55,17 @@ d3.csv("./assets/data/data.csv").then(function (stateStats) {
         .data(stateStats)
         .enter()
         .append("circle")
-        .attr("cx", (d,i) => xLinearScale(i))
-        .attr("cy", d => yLinearScale(d))
+        .attr("cx", (d) => xLinearScale(d.age))
+        .attr("cy", d => yLinearScale(d.smokes))
         .attr("r", "5")
         .attr("fill", "blue");
 
-    var toolTip = d3.select("body").append("div")
+    var toolTip = d3.tip()
         .attr("class", "tooltip");
 
     circlesGroup.call(toolTip);
+
+    // chartGroup.append("text")
 
 }).catch(function (e) {
     console.log(e);
